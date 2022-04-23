976786ddda900c138a4d583420e30a64703cb969
Nebulised <dhenleymarshall@gmail.com>
2022-04-23 08:36:09 +0100

    Temporarily disabled checkpoint and sample. Moved losses

--- core/solver.py
+++ core/solver.py
@@ -15,8 +15,8 @@ import datetime
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
+from core.losses import compute_d_loss, compute_g_loss
 from core.model import build_model
 from core.checkpoint import CheckpointIO
 from core.data_loader import InputFetcher
@@ -160,108 +160,41 @@ class Solver(nn.Module):
                 os.makedirs(args.sample_dir, exist_ok=True)
                 utils.debug_image(nets_ema, args, inputs=inputs_val, step=i+1)
 
-            # save model checkpoints
-            if (i+1) % args.save_every == 0:
-                self._save_checkpoint(step=i+1)
+            # # save model checkpoints
+            # if (i+1) % args.save_every == 0:
+            #     self._save_checkpoint(step=i+1)
+            #
+            # # compute FID and LPIPS if necessary
+            # if (i+1) % args.eval_every == 0:
+            #     calculate_metrics(nets_ema, args, i+1, mode='latent')
+            #     calculate_metrics(nets_ema, args, i+1, mode='reference')
 
-            # compute FID and LPIPS if necessary
-            if (i+1) % args.eval_every == 0:
-                calculate_metrics(nets_ema, args, i+1, mode='latent')
-                calculate_metrics(nets_ema, args, i+1, mode='reference')
-
-    @torch.no_grad()
-    def sample(self, loaders):
-        args = self.args
-        nets_ema = self.nets_ema
-        os.makedirs(args.result_dir, exist_ok=True)
-        self._load_checkpoint(args.resume_iter)
-
-        src = next(InputFetcher(loaders.src, None, args.latent_dim, 'test'))
-        ref = next(InputFetcher(loaders.ref, None, args.latent_dim, 'test'))
-
-        fname = ospj(args.result_dir, 'reference.jpg')
-        print('Working on {}...'.format(fname))
-        utils.translate_using_reference(nets_ema, args, src.x, ref.x, ref.y, fname)
-
-        fname = ospj(args.result_dir, 'video_ref.mp4')
-        print('Working on {}...'.format(fname))
-        utils.video_ref(nets_ema, args, src.x, ref.x, ref.y, fname)
-
-    @torch.no_grad()
-    def evaluate(self):
-        args = self.args
-        nets_ema = self.nets_ema
-        resume_iter = args.resume_iter
-        self._load_checkpoint(args.resume_iter)
-        calculate_metrics(nets_ema, args, step=resume_iter, mode='latent')
-        calculate_metrics(nets_ema, args, step=resume_iter, mode='reference')
-
-
-def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, masks=None):
-    assert (z_trg is None) != (x_ref is None)
-    # with real images
-    x_real.requires_grad_()
-    out = nets["discriminator"](x_real, y_org)
-    loss_real = adv_loss(out, 1)
-    loss_reg = r1_reg(out, x_real)
-
-    # with fake images
-    with torch.no_grad():
-        if z_trg is not None:
-            s_trg = nets["mapping_network"](z_trg, y_trg)
-        else:  # x_ref is not None
-            s_trg = nets["style_encoder"](x_ref, y_trg)
-
-        x_fake = nets["generator"](x_real, s_trg, masks=masks)
-    out = nets["discriminator"](x_fake, y_trg)
-    loss_fake = adv_loss(out, 0)
-
-    loss = loss_real + loss_fake + args.lambda_reg * loss_reg
-    return loss, {"real":loss_real.item(),"fake":loss_fake.item(),"reg" : loss_reg.item()}
-
-
-def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, masks=None):
-    assert (z_trgs is None) != (x_refs is None)
-    if z_trgs is not None:
-        z_trg, z_trg2 = z_trgs
-    if x_refs is not None:
-        x_ref, x_ref2 = x_refs
-
-    # adversarial loss
-    if z_trgs is not None:
-        s_trg = nets["mapping_network"](z_trg, y_trg)
-    else:
-        s_trg = nets["style_encoder"](x_ref, y_trg)
-
-    x_fake = nets["generator"](x_real, s_trg, masks=masks)
-    out = nets["discriminator"](x_fake, y_trg)
-    loss_adv = adv_loss(out, 1)
-
-    # style reconstruction loss
-    s_pred = nets["style_encoder"](x_fake, y_trg)
-    loss_sty = torch.mean(torch.abs(s_pred - s_trg))
-
-    # diversity sensitive loss
-    if z_trgs is not None:
-        s_trg2 = nets["mapping_network"](z_trg2, y_trg)
-    else:
-        s_trg2 = nets["style_encoder"](x_ref2, y_trg)
-    x_fake2 = nets["generator"](x_real, s_trg2, masks=masks)
-    x_fake2 = x_fake2.detach()
-    loss_ds = torch.mean(torch.abs(x_fake - x_fake2))
-
-    # cycle-consistency loss
-    masks = nets.fan.get_heatmap(x_fake) if args.w_hpf > 0 else None
-    s_org = nets["style_encoder"](x_real, y_org)
-    x_rec = nets["generator"](x_fake, s_org, masks=masks)
-    loss_cyc = torch.mean(torch.abs(x_rec - x_real))
-
-    loss = loss_adv + args.lambda_sty * loss_sty \
-        - args.lambda_ds * loss_ds + args.lambda_cyc * loss_cyc
-    return loss, {"adv": loss_adv.item(),
-                  "sty":loss_sty.item(),
-                  "ds":loss_ds.item(),
-                  "cyc":loss_cyc.item()}
+    # @torch.no_grad()
+    # def sample(self, loaders):
+    #     args = self.args
+    #     nets_ema = self.nets_ema
+    #     os.makedirs(args.result_dir, exist_ok=True)
+    #     self._load_checkpoint(args.resume_iter)
+    #
+    #     src = next(InputFetcher(loaders.src, None, args.latent_dim, 'test'))
+    #     ref = next(InputFetcher(loaders.ref, None, args.latent_dim, 'test'))
+    #
+    #     fname = ospj(args.result_dir, 'reference.jpg')
+    #     print('Working on {}...'.format(fname))
+    #     utils.translate_using_reference(nets_ema, args, src.x, ref.x, ref.y, fname)
+    #
+    #     fname = ospj(args.result_dir, 'video_ref.mp4')
+    #     print('Working on {}...'.format(fname))
+    #     utils.video_ref(nets_ema, args, src.x, ref.x, ref.y, fname)
+    #
+    # @torch.no_grad()
+    # def evaluate(self):
+    #     args = self.args
+    #     nets_ema = self.nets_ema
+    #     resume_iter = args.resume_iter
+    #     self._load_checkpoint(args.resume_iter)
+    #     calculate_metrics(nets_ema, args, step=resume_iter, mode='latent')
+    #     calculate_metrics(nets_ema, args, step=resume_iter, mode='reference')
 
 
 def moving_average(model, model_test, beta=0.999):
@@ -269,21 +202,3 @@ def moving_average(model, model_test, beta=0.999):
         param_test.data = torch.lerp(param.data, param_test.data, beta)
 
 
-def adv_loss(logits, target):
-    assert target in [1, 0]
-    targets = torch.full_like(logits, fill_value=target)
-    loss = F.binary_cross_entropy_with_logits(logits, targets)
-    return loss
-
-
-def r1_reg(d_out, x_in):
-    # zero-centered gradient penalty for real images
-    batch_size = x_in.size(0)
-    grad_dout = torch.autograd.grad(
-        outputs=d_out.sum(), inputs=x_in,
-        create_graph=True, retain_graph=True, only_inputs=True
-    )[0]
-    grad_dout2 = grad_dout.pow(2)
-    assert(grad_dout2.size() == x_in.size())
-    reg = 0.5 * grad_dout2.view(batch_size, -1).sum(1).mean(0)
-    return reg
