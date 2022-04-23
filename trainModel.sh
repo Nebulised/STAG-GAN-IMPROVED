a980479342fd234f0ffcb690c2c3d54c78e6e282
Nebulised <dhenleymarshall@gmail.com>
2022-04-23 08:37:38 +0100

    Bugfix - Incorrect var reference

--- trainModel.sh
+++ trainModel.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-imageSize=64
-numberClasses=3
+IMAGE_SIZE=64
+NUMBER_CLASSES=9
 SAMPLE_EVERY=500
 
 
 python3 main.py --img_size "$IMAGE_SIZE"\
- --num_domains "$numberClasses"\
+ --num_domains "$NUMBER_CLASSES"\
  --w_hpf 0\
+ --grayscale\
  --randcrop_prob 0.0\
  --train_img_dir "$1"\
  --val_img_dir "$2"\
  --mode "train"\
- --sample_every "$SAMPLE_EVERY"
+ --sample_every "$SAMPLE_EVERY"\
+
