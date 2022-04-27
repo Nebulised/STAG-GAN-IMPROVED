c3faef955df066c7e81d9e65d335a58ffb2de95a
Nebulised <dhenleymarshall@gmail.com>
2022-04-27 10:53:28 +0100

    Redundat masks removed, adaptive augmentation added

--- core/losses.py
+++ core/losses.py
@@ -2,13 +2,9 @@ import torch
 from torch.nn import functional as F
 
 
-def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, masks=None):
+def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, masks=None, adaptiveAug = None):
     assert (z_trg is None) != (x_ref is None)
     # with real images
-    x_real.requires_grad_()
-    out = nets["discriminator"](x_real, y_org)
-    loss_real = adv_loss(out, 1)
-    loss_reg = r1_reg(out, x_real)
 
     # with fake images
     with torch.no_grad():
@@ -18,14 +14,37 @@ def compute_d_loss(nets, args, x_real, y_org, y_trg, z_trg=None, x_ref=None, mas
             s_trg = nets["style_encoder"](x_ref, y_trg)
 
         x_fake = nets["generator"](x_real, s_trg, masks=masks)
-    out = nets["discriminator"](x_fake, y_trg)
+
+    x_real.requires_grad = True
+    if adaptiveAug is not None:
+        x_real_aug = adaptiveAug.forward(x_real)
+        x_fake_aug = adaptiveAug.forward(x_fake)
+    else:
+        x_real_aug = x_real
+        x_fake_aug = x_fake
+
+    #### DISCRIMINATOR ON REAL
+
+    # x_real.requires_grad()
+    out = nets["discriminator"](x_real_aug, y_org)
+    loss_real = adv_loss(out, 1)
+    loss_reg = r1_reg(out, x_real)
+    # DISCRIMINATOR ON FAKE
+
+    if adaptiveAug is not None:
+        with torch.no_grad():
+            adaptiveAug.update(out.clone().detach())
+
+    out = nets["discriminator"](x_fake_aug, y_trg)
     loss_fake = adv_loss(out, 0)
+
+
 
     loss = loss_real + loss_fake + args.lambda_reg * loss_reg
     return loss, {"real":loss_real.item(),"fake":loss_fake.item(),"reg" : loss_reg.item()}
 
 
-def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, masks=None):
+def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, masks=None, adaptiveAug = None):
     assert (z_trgs is None) != (x_refs is None)
     if z_trgs is not None:
         z_trg, z_trg2 = z_trgs
@@ -38,8 +57,13 @@ def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, m
     else:
         s_trg = nets["style_encoder"](x_ref, y_trg)
 
-    x_fake = nets["generator"](x_real, s_trg, masks=masks)
-    out = nets["discriminator"](x_fake, y_trg)
+    x_fake = nets["generator"](x_real, s_trg)
+    if adaptiveAug is not None:
+        x_fake_aug = adaptiveAug.forward(x_fake)
+
+    else:
+        x_fake_aug = x_fake
+    out = nets["discriminator"](x_fake_aug, y_trg)
     loss_adv = adv_loss(out, 1)
 
     # style reconstruction loss
@@ -51,14 +75,14 @@ def compute_g_loss(nets, args, x_real, y_org, y_trg, z_trgs=None, x_refs=None, m
         s_trg2 = nets["mapping_network"](z_trg2, y_trg)
     else:
         s_trg2 = nets["style_encoder"](x_ref2, y_trg)
-    x_fake2 = nets["generator"](x_real, s_trg2, masks=masks)
+    x_fake2 = nets["generator"](x_real, s_trg2)
     x_fake2 = x_fake2.detach()
     loss_ds = torch.mean(torch.abs(x_fake - x_fake2))
 
     # cycle-consistency loss
-    masks = nets.fan.get_heatmap(x_fake) if args.w_hpf > 0 else None
+
     s_org = nets["style_encoder"](x_real, y_org)
-    x_rec = nets["generator"](x_fake, s_org, masks=masks)
+    x_rec = nets["generator"](x_fake, s_org)
     loss_cyc = torch.mean(torch.abs(x_rec - x_real))
 
     loss = loss_adv + args.lambda_sty * loss_sty \
