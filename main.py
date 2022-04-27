9e2337a971f1fbbed854e608a8d23f3daf91d6b8
Nebulised <dhenleymarshall@gmail.com>
2022-04-27 21:19:19 +0100

    Changed default validation batch size

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                         help='Iterations to resume training/testing')
     parser.add_argument('--batch_size', type=int, default=8,
                         help='Batch size for training')
-    parser.add_argument('--val_batch_size', type=int, default=32,
+    parser.add_argument('--val_batch_size', type=int, default=8,
                         help='Batch size for validation')
     parser.add_argument('--lr', type=float, default=1e-4,
                         help='Learning rate for D, E and G')
