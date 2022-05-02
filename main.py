dc5939d158caa786049356abfe5d7fd6fa2dc6f3
Nebulised <dhenleymarshall@gmail.com>
2022-05-02 16:03:01 +0100

    Added layer wise comp args

--- main.py
+++ main.py
@@ -121,6 +121,17 @@ if __name__ == '__main__':
     parser.add_argument("--w_hpf",
                         default = 0,
                         type = int)
+    parser.add_argument("--layerWiseComposition",
+                        action = "store_true",
+                        default=False)
+
+    parser.add_argument("--lambda_sd_cyc",
+                        type = float,
+                        default = 5.0)
+
+    parser.add_argument("--lambda_sd_con",
+                        type = float,
+                        default = 1.0)
     parser.add_argument('--total_iters', type=int, default=100000,
                         help='Number of total iterations')
     parser.add_argument('--resume_iter', type=int, default=0,
